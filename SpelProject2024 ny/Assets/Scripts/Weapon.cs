37e979ec983d5d812a4c025078b44efb9dd9b731
localbaldy <filip.aadde@elev.ga.lbs.se>
2024-01-15 14:46:55 +0100

    ll

--- SpelProject2024 ny/Assets/Scripts/Weapon.cs
+++ SpelProject2024 ny/Assets/Scripts/Weapon.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public Transform firePoint;
+    public GameObject bulletPrefab;
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
+            Debug.Log("SHOOTI");
             Shoot();
         }
     }
     void Shoot()
     {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
-}
+}
---
+++ SpelProject2024 ny/Assets/Scripts/bullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public int damage = 40;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        Debug.Log("bullet here");
+        rb.velocity = transform.right * speed;
+    }
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
