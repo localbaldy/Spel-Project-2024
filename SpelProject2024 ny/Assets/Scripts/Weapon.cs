11727ed0d26d9bc6608d11d6ab496608f144a848
localbaldy <filip.aadde@elev.ga.lbs.se>
2024-01-16 13:44:15 +0100

    lallala

--- SpelProject2024 ny/Assets/Scripts/Weapon.cs
+++ SpelProject2024 ny/Assets/Scripts/Weapon.cs
@@ -1,24 +1,36 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 public class Weapon : MonoBehaviour
 {
     private Transform FirePoint;
     public GameObject bulletPrefab;
+    private float shootCooldown = 1.5f;  // Cooldown time in seconds
+    private bool canShoot = true;
+
     void Start()
     {
         FirePoint = GameObject.Find("FirePoint").transform;
     }
+
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && canShoot)
         {
-         
             Shoot();
+            StartCoroutine(ShootCooldown());
         }
     }
+
     void Shoot()
     {
         Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
     }
-}
+
+    IEnumerator ShootCooldown()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(shootCooldown);
+        canShoot = true;
+    }
+}
