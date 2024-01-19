82025f44a538b40b5604f521db5e88a61bf547db
TubeView <leo.delfinzon@elev.ga.lbs.se>
2024-01-19 10:50:57 +0100

    Fixed Controlls/End/Credits menu & Quit Game Button Added On Start Menu

---
+++ SpelProject2024 ny/Assets/Scripts/StartMenu2.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+{
+	Application.Quit();
+}
+
+}
